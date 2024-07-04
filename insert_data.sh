d59df4565c8d659e1fcc501d48f0c82a8bfa37ab
Michael <109044408+MightyCharm@users.noreply.github.com>
2024-07-04 17:58:23 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -53,7 +53,7 @@ do
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
   # get opponent_id
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-  # insert year
+  # insert data
   if [[ $YEAR != 'year' ]]
   then
     INSERT_DATA_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
